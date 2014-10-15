7a4a60f1bcbdb3b1aa59d0d79691a2b4821edb95
TJ Holowaychuk <tj@vision-media.ca>
2014-10-14 20:03:02 -0700

    add fs.ReadAllGzipLines(file string)

--- fs/fs.go
+++ fs/fs.go
@@ -2,6 +2,7 @@ package fs
 
 import "compress/gzip"
 import "io/ioutil"
+import "bufio"
 import "os"
 
 // OpenGzip opens gzipped `file` for reading.
@@ -22,4 +23,21 @@ func ReadAllGzip(file string) (b []byte, err error) {
 	}
 
 	return ioutil.ReadAll(r)
+}
+
+// ReadAllGzipLines reads all lines of gzipped `file`.
+func ReadAllGzipLines(file string) (lines [][]byte, err error) {
+	r, err := OpenGzip(file)
+	if err != nil {
+		return
+	}
+
+	scanner := bufio.NewScanner(r)
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Bytes())
+	}
+
+	err = scanner.Err()
+	return
 }
